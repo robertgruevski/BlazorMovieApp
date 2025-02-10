471718671e4490b0f02f8f7ed5df92524f3d3d85
robertgruevski <robertgruevski@outlook.com>
2025-02-10 16:28:24 -0500

    The movie class

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMovieApp.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+
+		public string? Title { get; set; }
+
+		public DateOnly ReleaseDate { get; set; }
+
+		public string? Genre { get; set; }
+
+		[DataType(DataType.Currency)]
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal Price { get; set; }
+	}
+}
